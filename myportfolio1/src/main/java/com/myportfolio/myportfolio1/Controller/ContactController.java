1568e76b67c7219b3df421211d0a09c6abbb967d
sanjay dhounidiyal <sanjudhounidiyalsanju@gmial.com>
2026-06-10 18:27:55 +0530

    Added update and delete API in ProjectController

--- myportfolio1/src/main/java/com/myportfolio/myportfolio1/Controller/ContactController.java
+++ myportfolio1/src/main/java/com/myportfolio/myportfolio1/Controller/ContactController.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/contact")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*")
 public class ContactController {
 
     @Autowired
